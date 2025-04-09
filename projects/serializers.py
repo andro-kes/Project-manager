6f32b35ad30a145e71c1ca982fda343f0a4118b3
Andrey Denisov <and.denisov26@gmail.com>
2025-04-09 14:32:55 +0300

    Прописал сериалайзеры

---
+++ projects/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Board, Task, Project
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username")
+        
+class TaskSerializer(serializers.ModelSerializer):
+    assigned_to = UserSerializer(read_only=True)
+    class Meta:
+        model = Task
+        fields = ("id", "title", "description", "priority", "created_at", "board", "assigned_to")
+        
+class BoardSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+    class Meta:
+        model = Board
+        fields = ("id", "project", "tasks")
+        
+class ProjectSerializer(serializers.ModelSerializer):
+    boards = BoardSerializer(read_only = True)
+    members = UserSerializer(many=True, read_only = True)
+    team_lead = UserSerializer(read_only=True)
+    class Meta:
+        model = Project
+        fields = ("id", "title", "description", "boards", "team_lead", "members")

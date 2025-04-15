91114fdf46366c58f66ead6a8624c056ce084af3
Andrey Denisov <and.denisov26@gmail.com>
2025-04-15 18:51:18 +0300

    Написал API по получению и созданию проектов и досок

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Board, Project
+from .serializers import ProjectSerializer, BoardSerializer
 
-# Create your views here.
+class BaseProjectAPIView(generics.GenericAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class CreateProjectAPIView(BaseProjectAPIView, generics.CreateAPIView):
+    def perform_create(self, serializer):
+        project = serializer.save()
+        Board.objects.create(project=project)
+    
+class GetProjectAPIView(BaseProjectAPIView, generics.RetrieveAPIView):
+    pass
+
+class GetBoardAPIView(generics.RetrieveAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer    

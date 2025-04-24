75b5476039cff8baab7bb75dba47ab53c5643403
Andrey Denisov <and.denisov26@gmail.com>
2025-04-24 20:14:46 +0300

    Исправил опечатку

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("projects/create", views.CreateProjectAPIView.as_view(), name="create_project"),
     path("projects/<int:pk>", views.RetrieveProjectAPIView.as_view(), name='get_project'),
     path("projects/board/<int:pk>", views.GetBoardAPIView.as_view(), name="get_board"),
-    path("projects/<int:board_id>/tasks/create", views.CreateTaskAPIView.as_view(), name="cteate_task"),
+    path("projects/<int:board_id>/tasks/create", views.CreateTaskAPIView.as_view(), name="create_task"),
     path("projects/board/<int:board_id>/tasks/<int:pk>/update", views.UpdateTaskAPIView.as_view(), name="update_task"),
     path("projects/board/<int:board_id>/tasks/<int:pk>/delete", views.DeleteTaskAPIView.as_view(), name="delete_task"),
 ]

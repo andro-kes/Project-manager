6600320a61b8639bc1bb034cad492147e0e185d8
GoldenDime <hidima78@gmail.com>
2025-05-27 23:06:50 +0300

    profiles

---
+++ profiles/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from users.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    skills = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    tg = models.CharField(max_length=100, blank=True)
+    #avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
---
+++ profiles/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from profiles.models import Profile
+from users.serializers import UserSerializer
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'bio', 'skills', 'phone']#, 'avatar'
+        read_only_fields = ['id', 'user']
+
+
+class ProfileUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'skills', 'phone'] #, 'avatar'
